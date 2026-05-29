aa5b1d54ad86e53330f03db475bc1488b9cd9dd6
ZvRzyan18 <rhenzalfred06@gmail.com>
2026-05-30 07:05:54 +0800

    update files

--- include/zac/pipelines/gui_textured_pannel_pipeline.h
+++ include/zac/pipelines/gui_textured_pannel_pipeline.h
@@ -1,5 +1,11 @@
 #ifndef ZAC_GUI_TEXTURED_PANNEL_PIPELINE_H
 #define ZAC_GUI_TEXTURED_PANNEL_PIPELINE_H
 
+#include "zac/renderer/ctxrender.h"
+#include "zac/renderer/pipelines.h"
+
+void __ZAC_CreateTexturedPannelPipeline(ZAC_Ctxrender *ctx, ZAC_Pipelines *p, uintptr_t shaders[2], size_t shader_size[2], size_t pc_size);
+
+
 #endif
 
